038ee58813a5bf51df866a811dee97c8bc5a0fc9
Madhankumar Dorai <Madhankumar.dorai@oneadvanced.com>
2018-12-19 05:00:58 +0000

    Add project files.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,443 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+            Dictionary<Guid, List<SQLStageTime>> connDictionary = new Dictionary<Guid, List<SQLStageTime>>();
+            List<NotClosedNotDisposed> opeNotClosedNotDisposed = new List<NotClosedNotDisposed>();
+            List<NotClosed> openNotClosed = new List<NotClosed>();
+            List<NotDisposed> openNotDisposed = new List<NotDisposed>();
+            List<ConnectionTimers> connectionTimers = new List<ConnectionTimers>();
+            using (StreamReader sr = File.OpenText(@"C:\Users\Madhankumar.dorai\Desktop\PerformanceLogger_20181217.log"))
+            {
+                string TimeStamp = "";
+                string stage = "";
+                string Id = "";
+                string Hash = "";
+                string root = "";
+                string origin = "";
+                string s = "";
+                bool blockEnded = false;
+                while ((s = sr.ReadLine()) != null)
+                {
+                    if (s.Contains("INFO Performance -"))
+                    {
+                        TimeStamp = s.Substring(0, s.IndexOf('[') - 5);
+                    }
+                    else if (s.Contains("Stage -"))
+                    {
+                        stage = s.Substring(s.IndexOf("Stage - ") + 8);
+                    }
+                    else if (s.Contains("Id -"))
+                    {
+                        Id = s.Substring(s.IndexOf("Id - ") + 5);
+                    }
+                    else if (s.Contains("Hash -"))
+                    {
+                        Hash = s.Substring((s.IndexOf("Hash - ") + 7));
+                        //blockEnded = true;
+                    }
+                    else if (s.Contains("Root -"))
+                    {
+                        root = s.Substring((s.IndexOf("Root - ") + 7));
+                        //blockEnded = true;
+                    }
+                    else if (!string.IsNullOrEmpty(root) && s.Contains(root))
+                    {
+                        origin = s;
+                        blockEnded = true;
+                    }
+
+                    if (blockEnded)
+                    {
+                        Guid identifier = Guid.Empty;
+                        if (Guid.TryParse(Id, out identifier))
+                        {
+                            if (connDictionary.ContainsKey(identifier))
+                            {
+                                int order = connDictionary[identifier].OrderBy(m => m.Order).Last().Order;
+                                connDictionary[identifier].Add(new SQLStageTime { Order = order + 1, Stage = stage, Time = DateTime.Parse(TimeStamp), Origin = origin, Root = root });
+                            }
+                            else
+                            {
+                                connDictionary.Add(identifier, new List<SQLStageTime> { new SQLStageTime { Order = 1, Stage = stage, Time = DateTime.Parse(TimeStamp), Origin = origin, Root = root } });
+                            }
+                        }
+                        root = null;
+                        blockEnded = false;
+                    }
+
+                    //list.Add(s);
+                }
+            }
+
+            foreach (var ids in connDictionary)
+            {
+                var key = ids.Key;
+                var transactions = ids.Value;
+
+                DateTime openTime = new DateTime();
+                DateTime closeTime = new DateTime();
+                DateTime disposeTime = new DateTime();
+
+                var openTransactionCount = transactions.Count(m => m.Stage == "Open Connection");
+                var closedTransactionCount = transactions.Count(m => m.Stage == "Close Connection");
+                var disposedTransactionCount = transactions.Count(m => m.Stage == "Dispose Connection");
+
+
+                if (openTransactionCount == closedTransactionCount &&
+                    closedTransactionCount == disposedTransactionCount) // Properly Managed
+                {
+                    foreach (var sqlStageTime in transactions.OrderBy(m => m.Order))
+                    {
+                        if (sqlStageTime.Stage == "Open")
+                        {
+                            openTime = sqlStageTime.Time;
+                            continue;
+                        }
+
+                        if (sqlStageTime.Stage == "Close")
+                        {
+                            closeTime = sqlStageTime.Time;
+                            continue;
+                        }
+
+                        if (sqlStageTime.Stage == "Dispose")
+                        {
+                            disposeTime = sqlStageTime.Time;
+                        }
+
+                        connectionTimers.Add(new ConnectionTimers
+                        {
+                            Guid = key,
+                            CloseDuration = closeTime - openTime,
+                            DisposedDuration = disposeTime - closeTime,
+                            TotalElapsedDuration = disposeTime - openTime
+                        });
+
+                    }
+                }
+                else if (openTransactionCount != closedTransactionCount && openTransactionCount != disposedTransactionCount) //open but not closed and disposed
+                {
+                    var stringlist = new List<string>();
+                    stringlist.Add("Stage connections open but not closed and disposed");
+                    foreach (var trans in transactions.OrderBy(m => m.Order))
+                    {
+                        stringlist.Add(trans.Stage + ":" + trans.Root + ":" + trans.Origin);
+                    }
+
+                    opeNotClosedNotDisposed.Add(new NotClosedNotDisposed
+                    {
+                        Guid = key,
+                        Message = stringlist
+                    });
+                }
+                else if (openTransactionCount != closedTransactionCount) //open but not closed
+                {
+                    var stringlist = new List<string>();
+                    stringlist.Add("Stage connections open but not closed");
+                    foreach (var trans in transactions.OrderBy(m => m.Order))
+                    {
+                        stringlist.Add(trans.Stage + ":" + trans.Root + ":" + trans.Origin);
+                    }
+                    openNotClosed.Add(new NotClosed
+                    {
+                        Guid = key,
+                        Message = stringlist
+                    });
+                }
+                else if (openTransactionCount != disposedTransactionCount) //open but not disposed
+                {
+                    var stringlist = new List<string>();
+                    stringlist.Add("Stage connections open but not disposed");
+                    foreach (var trans in transactions.OrderBy(m => m.Order))
+                    {
+                        stringlist.Add(trans.Stage + ":" + trans.Root + ":" + trans.Origin);
+                    }
+                    openNotDisposed.Add(new NotDisposed
+                    {
+                        Guid = key,
+                        Message = stringlist
+                    });
+                }
+
+                //if (openTransactionCount != closedTransactionCount || openTransactionCount != disposedTransactionCount || closedTransactionCount != disposedTransactionCount)
+                //{
+                //    opeNotClosedNotDisposed.Add(new NotClosedNotDisposed
+                //    {
+                //        Guid = key,
+                //        Message = "Stage connections do not match"
+                //    });
+                //}
+                //else
+                //{
+                //    foreach (var sqlStageTime in transactions.OrderBy(m => m.Order))
+                //    {
+                //        if (sqlStageTime.Stage == "Open")
+                //        {
+                //            openTime = sqlStageTime.Time;
+                //            continue;
+                //        }
+
+                //        if (sqlStageTime.Stage == "Close")
+                //        {
+                //            closeTime = sqlStageTime.Time;
+                //            continue;
+                //        }
+
+                //        if (sqlStageTime.Stage == "Dispose")
+                //        {
+                //            disposeTime = sqlStageTime.Time;
+                //        }
+
+                //        connectionTimers.Add(new ConnectionTimers
+                //        {
+                //            Guid = key,
+                //            CloseDuration = closeTime - openTime,
+                //            DisposedDuration = disposeTime - closeTime,
+                //            TotalElapsedDuration = disposeTime - openTime
+                //        });
+
+                //    }
+                //}
+
+            }
+
+
+
+            //foreach (var conn in connDictionary)
+            //{
+            //    var key = conn.Key;
+            //    var transactions = conn.Value;
+
+            //    DateTime openTime = new DateTime();
+            //    DateTime closeTime = new DateTime();
+            //    DateTime disposeTime = new DateTime();
+
+
+
+            //    foreach (var sqlStageTime in transactions.OrderBy(m => m.Order))
+            //    {
+            //        if (sqlStageTime.Stage == "Open")
+            //        {
+            //            openTime = sqlStageTime.Time;
+            //            continue;
+            //        }
+
+            //        if (sqlStageTime.Stage == "Close")
+            //        {
+            //            closeTime = sqlStageTime.Time;
+            //            continue;
+            //        }
+
+            //        if (sqlStageTime.Stage == "Dispose")
+            //        {
+            //            disposeTime = sqlStageTime.Time;
+            //        }
+
+            //        connectionTimers.Add(new ConnectionTimers
+            //        {
+            //            Guid = key,
+            //            CloseDuration = openTime - closeTime
+            //        });
+
+            //    }
+            //}
+
+
+            Console.WriteLine("Processing connections not disposed and not closed count: " + opeNotClosedNotDisposed.Count);
+            Console.WriteLine("Press Enter to continue display the stack trace");
+            Console.ReadLine();
+            foreach (var unMaintainedConnection in opeNotClosedNotDisposed)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("UnManaged Connections");
+                Console.WriteLine(unMaintainedConnection.Guid);
+                Console.ResetColor();
+                foreach (var msg in unMaintainedConnection.Message)
+                {
+                    Console.WriteLine(msg);
+                }
+            }
+
+            var consolidatedRes = opeNotClosedNotDisposed.SelectMany(m => m.Message).ToList();
+
+            var groupedRes = from p in consolidatedRes
+                             where p != "Stage connections open but not closed and disposed"
+                             group p by p;
+
+            Console.WriteLine("Consolidated messages");
+
+            foreach (var c in groupedRes.OrderByDescending(m => m.Count()))
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine(c.FirstOrDefault());
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Count: " + c.Count());
+            }
+
+            Console.ResetColor();
+
+            Console.WriteLine("Processing connections not closed count: " + openNotClosed.Count);
+            Console.WriteLine("Press Enter to continue display the stack trace");
+            Console.ReadLine();
+
+            foreach (var unMaintainedConnection in openNotClosed)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Open but not closed");
+                Console.WriteLine(unMaintainedConnection.Guid);
+                Console.ResetColor();
+                foreach (var msg in unMaintainedConnection.Message)
+                {
+                    Console.WriteLine(msg);
+                }
+            }
+
+            var consolidatedRes2 = openNotClosed.SelectMany(m => m.Message).ToList();
+
+            var groupedRes2 = from p in consolidatedRes2
+                              where p != "Stage connections open but not closed"
+                              group p by p;
+
+            Console.WriteLine("Consolidated messages");
+
+            foreach (var c in groupedRes2.OrderByDescending(m => m.Count()))
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine(c.FirstOrDefault());
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Count: " + c.Count());
+            }
+
+            Console.ResetColor();
+
+            Console.WriteLine("Processing connections not disposed count: " + openNotDisposed.Count);
+            Console.WriteLine("Press Enter to continue display the stack trace");
+            Console.ReadLine();
+
+            foreach (var unMaintainedConnection in openNotDisposed)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Open but not disposed");
+                Console.WriteLine(unMaintainedConnection.Guid);
+                Console.ResetColor();
+
+                foreach (var msg in unMaintainedConnection.Message)
+                {
+                    Console.WriteLine(msg);
+                }
+            }
+
+            var consolidatedRes3 = openNotDisposed.SelectMany(m => m.Message).ToList();
+
+            var groupedRes3 = from p in consolidatedRes3
+                where p != "Stage connections open but not disposed"
+                              group p by p;
+
+            Console.WriteLine("Consolidated messages");
+
+            foreach (var c in groupedRes3.OrderByDescending(m => m.Count()))
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine(c.FirstOrDefault());
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Count: " + c.Count());
+            }
+
+            Console.ResetColor();
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Connection Durations");
+            Console.ResetColor();
+
+            foreach (var connectionTimer in connectionTimers.OrderByDescending(m => m.CloseDuration).ThenByDescending(m => m.DisposedDuration).ThenByDescending(m => m.TotalElapsedDuration))
+            {
+                Console.WriteLine(connectionTimer.Guid);
+                Console.WriteLine("Closed Duration :" + connectionTimer.CloseDuration);
+                Console.WriteLine("Disposed Duration :" + connectionTimer.DisposedDuration);
+                Console.WriteLine("Elapsed Duration :" + connectionTimer.TotalElapsedDuration);
+            }
+
+
+            Console.ReadLine();
+
+            //foreach (var VARIABLE in list)
+            //{
+            //    string iden = VARIABLE.Substring(VARIABLE.IndexOf("ConnectionId - ") + 15, 36);
+            //    Guid identifier = Guid.Empty;
+            //    if(Guid.TryParse(iden, out identifier))
+            //    {
+            //        if (connDictionary.ContainsKey(identifier))
+            //        {
+            //            string stag = VARIABLE.Substring(VARIABLE.IndexOf("- Stage - ") + 10, VARIABLE.IndexOf(" ConnectionId - "));
+            //            connDictionary[identifier].Add(stag);
+            //        }
+            //        else
+            //        {
+            //            connDictionary.Add(identifier, new List<string>{ VARIABLE.Substring(VARIABLE.IndexOf("- Stage - ") + 10, VARIABLE.IndexOf(" ConnectionId - ")) });
+            //        }
+            //    }
+            //}
+
+        }
+    }
+
+    class SQLStageTime
+    {
+        public int Order { get; set; }
+        public string Stage { get; set; }
+        public DateTime Time { get; set; }
+        public string Root { get; set; }
+        public string Origin { get; set; }
+    }
+
+    class NotClosedNotDisposed
+    {
+        public NotClosedNotDisposed()
+        {
+            Message = new List<string>();
+        }
+        public Guid Guid { get; set; }
+        public List<string> Message { get; set; }
+    }
+
+    class NotClosed
+    {
+        public NotClosed()
+        {
+            Message = new List<string>();
+        }
+        public Guid Guid { get; set; }
+        public List<string> Message { get; set; }
+    }
+
+    class NotDisposed
+    {
+        public NotDisposed()
+        {
+            Message = new List<string>();
+        }
+        public Guid Guid { get; set; }
+        public List<string> Message { get; set; }
+    }
+
+    class ConnectionTimers
+    {
+        public Guid Guid { get; set; }
+        public TimeSpan CloseDuration { get; set; }
+        public TimeSpan DisposedDuration { get; set; }
+        public TimeSpan TotalElapsedDuration { get; set; }
+    }
+}
